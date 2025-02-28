ebbe6854f8565c4761e1ce720cc98e84f334fb7a
Greg W <1514406+fatbotgw@users.noreply.github.com>
2025-02-27 22:28:35 -0500

    Added dict_sort() and some output formatting in main()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import word_count
 from stats import character_count
+from stats import dict_sort
 
 def get_book_test(path_to_file):
     with open(path_to_file) as f:
@@ -13,7 +14,18 @@ def main():
     book = get_book_test(book_location)
     words = word_count(book)
 
-    print(f"{words} words found in the document")
-    print(character_count(book))
+    word_list = dict_sort(character_count(book))
+    # print(f"{words} words found in the document")
+    # print(character_count(book))
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for word in word_list:
+        for key in word:
+            print(f"{key}: {word[key]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,7 +9,6 @@ def word_count(book_text):
 
 
 def character_count(text_string):
-    pass
     counts = {}
 
     for letter in text_string:
@@ -19,3 +18,29 @@ def character_count(text_string):
             counts[letter.lower()] = 1
 
     return counts
+
+
+def sort_on(dict_item):
+    # Get the first (and only) key in the dictionary
+    key = list(dict_item.keys())[0]
+    # Return the value associated with that key
+    return dict_item[key]
+
+
+def dict_sort(dictionary):
+    sorted_list = []        # this is a list of dictionaries
+
+    # This works, but isn't very Python-like
+    # for entry in dictionary:
+    #     if entry.isalpha() is True:
+    #         sorted_list.append({entry:dictionary[entry]})
+
+    # This way of iterating through key-value pairs is the more Python way
+    for key, value in dictionary.items():
+        if key.isalpha() is True:
+            sorted_list.append({key: value})
+
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list#.sort(key=sort_on(), reverse=True)
